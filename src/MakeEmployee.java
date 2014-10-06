f5e0a7866983a2ae618485b57c43f803164030e7
cait2290 <cait2290@ISS147D8-25.scdsb.pri>
2014-10-06 08:45:27 -0400

    October 6

--- src/MakeEmployee.java
+++ src/MakeEmployee.java
@@ -1,8 +1,25 @@
+import TerminalIO.*;
 
 public class MakeEmployee {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) 
+    {
+        KeyboardReader r= new KeyboardReader();
+       
+        Employee emp;
+        String name;
+        int type;
+        double rate;
+        int hours;
+        String prompt;
+        
+        emp=new Employee();
+        
+        while(true)
+        {
+           //get name and break if its blank 
+           System.out.println("Enter Employee data");
+        }
     }
     
 }
