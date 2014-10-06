aaba86cdaf870ac4a0035dc9cdecd82e4e6a97cf
cait2290 <cait2290@ISS147D8-25.scdsb.pri>
2014-10-06 08:18:44 -0400

    October 6

--- src/Employee.java
+++ src/Employee.java
@@ -32,4 +32,65 @@ public class Employee
     {
         return "between 1 and 60, inclusive";
     }
+    
+    public boolean setName(String nm)
+    {
+        boolean blank= (nm.equals(""));
+        if (blank) return false;
+        else
+        {
+           name=nm;
+           return true;
+        }
+    }
+    
+    public boolean setType(int tp)
+    {
+        boolean oneortwo= (tp==1 || tp==2);
+        if (oneortwo)
+        {
+            type=tp;
+            return true;
+        }
+        else return false;
+    } 
+    
+     public boolean setRate(double rt)
+    {
+        boolean rateok= (rt>=6.75 && rt<=30.50);
+        if (rateok)
+        {
+            rate=rt;
+            return true;
+        }
+        else return false;
+    } 
+     
+      public boolean setHours(int hrs)
+    {
+        boolean hoursok= (hrs>=1 && hrs<=60);
+        if (hoursok)
+        {
+            hours=hrs;
+            return true;
+        }
+        else return false;
+    } 
+      
+      public String getName()
+      {
+          return name;
+      }
+      
+      public double getPay()
+      {
+          double pay;
+          boolean noovertime=(hours<=40 || type==2);
+          if (noovertime)
+              pay=rate*hours;
+          
+          else
+              pay=(hours-40)* (rate*2)+rate*40;
+          return pay;
+      }
 }
